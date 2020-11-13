78f4c3b8240413625db58ccd715511e371f7eed3
fulongfei <lffu_0032@outlook.com>
2020-11-13 09:14:20 +0800

    update

--- kmerFusion.pl
+++ kmerFusion.pl
@@ -4,70 +4,103 @@ use File::Basename;
 use FindBin qw/$Bin/;
 use Getopt::Long;
 
-my ($bam,$sampleName,$fa_ref,$annot_file,$main_NM,$chrNaming,$outdir);
+my ($bam,$sampleName,$fa_ref,$padding_len,$outdir);
 
 GetOptions(
-    "bam:s" => \$bam,                  # Need
-    "n:s" => \$sampleName,             # Need
-    "fa:s" => \$fa_ref,                # Default: /data1/database/b37/human_g1k_v37.fasta
-    "annot:s" => \$annot_file,         # Default: /home/fulongfei/workdir/git_repo/GeExCNV/public_db/refFlat.txt
-    "NM:s" => \$main_NM,               # Default: /home/fulongfei/workdir/git_repo/GeExCNV/public_db/MainNM_yrt_20191111.txt
-    "chrName:s" => \$chrNaming,        # Default: no_chr_prefix
-    "od:s" => \$outdir,                # Need
-    ) or die "unknown args\n";
-
+    "bam:s"         => \$bam,                   # Need
+    "n:s"           => \$sampleName,            # Need
+    "fa:s"          => \$fa_ref,                # Default: </data1/database/b37/human_g1k_v37.fasta>
+    "Plen:i"        => \$padding_len,           # Default: 1000
+    "od:s"          => \$outdir,                # Need
+    ) or die;
 
 # default value
 if (not defined $fa_ref){
     $fa_ref = "/data1/database/b37/human_g1k_v37.fasta";
 }
 
-if (not defined $annot_file){
-    $annot_file = "/home/fulongfei/workdir/git_repo/GeExCNV/public_db/refFlat.txt";
+if (not defined $padding_len){
+    $padding_len = 1000;
 }
 
-if (not defined $main_NM){
-    $main_NM = "/home/fulongfei/workdir/git_repo/GeExCNV/public_db/MainNM_yrt_20191111.txt";
-}
+my $soft_clip_count = 2;
 
-if (not defined $chrNaming){
-    $chrNaming = "no_chr_prefix";
-}
+my $runsh = "$od/gfuse\.$sampleName\.sh";
+open RUNSH, ">$runsh" or die;
 
+# bam2fq
+my $cmd = "perl $Bin/bin/bam2fq.pl $bam $sampleName $outdir";
+#system($cmd);
+print RUNSH "$cmd\n";
 
-# check outdir
-if (!-d $outdir){
-    `mkdir -p $outdir`;
-}
+# get soft-clip region info
+my $soft_clip_pos_file = "$outdir/$sampleName\.softclip.pos.txt";
+$cmd = "samtools view $bam | awk '\$6~/S/ {print \$3\"\\t\"\$4}\' >$soft_clip_pos_file";
+#system($cmd);
+print RUNSH "$cmd\n";
 
-#my $runsh = "$outdir/$sampleName\.genefuse.sh";
-my $runsh = "$outdir/genefuse\.$sampleName\.sh";
-open O, ">$runsh" or die;
+# filter soft clip pos
+# if a pos has <= 2 clip reads, then this pos will be filtered out
+$cmd = "perl $Bin/bin/filter_sv_pos.pl $soft_clip_pos_file $sampleName $outdir";
+#system($cmd);
+print RUNSH "$cmd\n";
 
-# convert bam into fq using bedtools bamtofastq
-# sort by name, not by coord
-my $bam_sort_by_name = "$outdir/$sampleName\.sort_by_name.bam";
-my $cmd = "samtools sort -n -o $bam_sort_by_name $bam";
-print O "$cmd\n";
+# make gene bed
+my $cosmicGene = "$Bin/public_db/cosmic_transcript.txt.gz";
+my $ensGeneAnnot = "$Bin/public_db/hg19_ensGene.txt.gz";
+my $chr_naming = &check_chr_naming($fa_ref);
+$cmd = "perl $Bin/bin/make_gene_bed.pl $cosmicGene $ensGeneAnnot $chr_naming $outdir";
+#system($cmd);
+print RUNSH "$cmd\n";
 
-# bamtofastq
+# annot soft clip pos with gene name using bedtools
+my $all_gene_bed = "$outdir/all.gene.bed";
+$cmd = "bedtools intersect -a $pass_filter_pos_file -b $all_gene_bed -wo -bed >$outdir/sampleName\.softclip.pos.PASS.annot.Gene.txt";
+#system($cmd);
+print RUNSH "$cmd\n";
+
+# make fusion csv file
+$cmd = "perl $Bin/bin/make_fusion_list.pl $cosmicGene $ensGeneAnnot $outdir/sampleName\.softclip.pos.PASS.annot.Gene.txt $chr_naming $outdir";
+#system($cmd);
+print RUNSH "$cmd\n";
+
+# call genefuse to detect fusion gene
+my $flist = "$outdir/fusion.csv";
 my $fq1 = "$outdir/$sampleName\.R1.fastq";
 my $fq2 = "$outdir/$sampleName\.R2.fastq";
 
-$cmd = "bedtools bamtofastq -i $bam_sort_by_name -fq $fq1 -fq2 $fq2";
-print O "$cmd\n";
+$cmd = "$Bin/genefuse -t 12 -r $fa_ref -f $flist -1 $fq1 -2 $fq2 -h $outdir/$sampleName\.genefuse.html -j $outdir/$sampleName\.genefuse.json";
+#system($cmd);
+print RUNSH "$cmd\n";
 
-# make csv list
-my $bed = "$Bin/bin/all_genes.bed";
-$cmd = "bedtools intersect -abam $bam -b $bed -bed -wo >$outdir/covered_genes.txt";
-print O "$cmd\n";
+# get fusion result from json file
+my $json = "$outdir/$sampleName\.genefuse.json";
+$cmd = "perl $Bin/bin/get_final_fusion_result.pl $json $sampleName $outdir";
+#system($cmd);
+print RUNSH "$cmd\n";
 
-my $fusion_list = "$outdir/fusion_list.csv";
-$cmd = "perl $Bin/bin/make_genefuse_csv_list.pl -gene $outdir/covered_genes.txt -annot $annot_file -NM $main_NM -chrname $chrNaming -of $fusion_list";
-print O "$cmd\n";
+# stat genefuse result info
 
-# run genefuse
-$cmd = "$Bin/genefuse -r $fa_ref -f $fusion_list -1 $fq1 -2 $fq2 -h $outdir/$sampleName\.genefuse.html -j $outdir/$sampleName\.genefuse.json";
-print O "$cmd\n";
 
-close O;
+close RUNSH;
+
+
+
+
+sub check_chr_naming{
+    my ($fa) = @_;
+    open IN, "$fa" or die;
+    my $first_line = <IN>;
+    close IN;
+    
+    my $naming;
+    if ($first_line =~ /^>chr/){
+        $naming = "with_chr_prefix";
+    }else{
+        $naming = "no_chr_prefix";
+    }
+
+    return($naming);
+}
+
+
