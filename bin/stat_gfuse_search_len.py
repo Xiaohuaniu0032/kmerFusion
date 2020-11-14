fc98344474bbf2d526f1c013c327653932939eaa
fulongfei <lffu_0032@outlook.com>
2020-11-14 19:42:56 +0800

    update

--- bin/stat_gfuse_search_len.py
+++ bin/stat_gfuse_search_len.py
@@ -1,9 +1,12 @@
 import os
 import sys
+from collections import defaultdict
 
 fusion_csv = sys.argv[1]
 
 fcsv = open(fusion_csv,'r')
+gene2len = defaultdict(int)
+
 total_len = 0
 for i in fcsv:
 	if '>' in i:
@@ -12,9 +15,17 @@ for i in fcsv:
 		start = int(reg.split('-')[0])
 		end = int(reg.split('-')[1])
 		reg_len = end - start + 1
-		total_len += reg_len
-		val = "gene %s len is:%s" % (gene,reg_len)
-		print(val)
+		gene2len[gene] += reg_len
+		#total_len += reg_len
+		#val = "gene %s len is:%s" % (gene,reg_len)
+		#print(val)
+
+total_len = 0;
+for gene,n in gene2len.items():
+	#print(gene,n)
+	val = "gene %s len is: %s" % (gene,n)
+	print(val)
+	total_len += n
 
 val = "total search len is: %s(bp)" % (total_len)
 print(val)
