ed9c154da5a3a86c5d3c2f90ce4b06d6a6acddf7
fulongfei <lffu_0032@outlook.com>
2020-12-28 15:11:07 +0800

    update

--- bin/check_split_read_uniq.py
+++ bin/check_split_read_uniq.py
@@ -2,12 +2,14 @@ import pysam
 import sys
 import argparse
 import re
+import os
 
 def parse_args():
     AP = argparse.ArgumentParser("check the uniqness of alignment of fusion support split reads")
     AP.add_argument('-bam',help='tumor bam',dest='bam')
+    #AP.add_argument('-n',help='sample name',dest='name')
     AP.add_argument('-f',help='*.fusion.xls file',dest='fsFile')
-    AP.add_argument('-mpq',help='map quality cutoff',dest='mpq',default=30)
+    AP.add_argument('-mq',help='map quality cutoff',dest='mapq',default=60)
     AP.add_argument('-refFlag',help='refFlat to annot gene strand info',dest='refFlat',default='/home/fulongfei/workdir/git_repo/GeExCNV/public_db/refFlat.txt')
     AP.add_argument('-od',help='output dir',dest='outdir')
 
@@ -17,7 +19,13 @@ def parse_args():
 def main():
     args = parse_args()
     fsFH = open(args.fsFile,'r')
+    name = os.path.basename(args.bam).split('.')[0]
+    outfile = '%s/%s.sr_mapq_uniqness_check.txt' % (args.outdir,name)
 
+    ofFH = open(outfile,'w')
+    # fusion gene | gene | gene_type | read name | READ1/2 | chr | sv_pos | split_read_pos | pos_check | SR MAPQ | mapQ check | final result (check pos and mapq)
+    outfile_header = ['fusionGene','Gene','GeneType(Hgene or Tgene)','read_name','READ1/2','chr','sv_pos','sr_pos','pos_check','sr_mapq','sr_mapq_check','final_check']
+    ofFH.write('\t'.join(outfile_header)+'\n') # write outfile header
 
     # get gene strand info
     gene_strand = {}
@@ -41,19 +49,31 @@ def main():
 
         print("[INFO]: check %s fusion info..." % (arr[1]))
 
-        hgene = arr[1].split('->')[0]
-        tgene = arr[1].split('->')[1]
+        hgene = arr[1].split('-')[0] # not use ->
+        tgene = arr[1].split('-')[1]
+
+        # fs chrom info
+        fs_chr = {}
+        fs_chr[arr[11]] = arr[12] # gene => chr
+        fs_chr[arr[17]] = arr[18]
+
 
         # fusion gene pos info
         fs_info = {}
-        fs_info[arr[10]] = int(arr[12]) # gene -> pos
-        fs_info[arr[16]] = int(arr[18])
+        fs_info[arr[11]] = int(arr[13]) # gene -> pos
+        fs_info[arr[17]] = int(arr[19])
 
         samfile = pysam.AlignmentFile(args.bam,'rb')
         
-        bp1_left = int(arr[12]) - 10
-        bp1_right = int(arr[12]) + 10
+        bp1_left = int(arr[13]) - 10
+        bp1_right = int(arr[13]) + 10
 
+
+        # 统计hgene/tgene支持split read的SA MAPQ
+        # sv断点在5bp以内的primary read (with SA tag)，算作一条有效fusion-support read
+        # 统计有效read的MAPQ，>=60 MAPQ占比需>=90%
+
+        hgene_mapQ = [] # 
         
         hgene_strand = gene_strand[hgene]
         print("[INFO]: check Hgene %s (%s) info..." % (hgene,hgene_strand))
@@ -63,10 +83,14 @@ def main():
         hgene_left = hgene_bp - 10
         hgene_right = hgene_bp + 10
 
+        #print(hgene,hgene_left,hgene_right)
+
         hgene_strand = gene_strand[hgene]
         tgene_strand = gene_strand[tgene]
 
-        for read in samfile.fetch(arr[11],hgene_left,hgene_right):
+        partner_chr = fs_chr[tgene]
+
+        for read in samfile.fetch(arr[12],hgene_left,hgene_right):
             if read.is_read1:
                 r1r2 = 'READ1'
             else:
@@ -95,9 +119,15 @@ def main():
                     continue
 
                 # skip mapQ < 10
-                mapQ = int(sa_tag.split(',')[4])
-                if mapQ < 10:
+                sr_mapq = int(sa_tag.split(',')[4])
+                if sr_mapq < 10:
                     continue
+
+                # check SA's chr
+                sa_chr = sa_tag.split(',')[0]
+                if sa_chr != partner_chr:
+                    continue
+
 
                 # check Hgene's sup read's ori
                 if hgene_strand == '+' and tgene_strand == '-':
@@ -153,6 +183,7 @@ def main():
                     if read_rev == 'FORWARD':
                         # split read need on '+'
                         if sa_strand == '+':
+                            #print(read.query_name)
                             pass
                         else:
                             continue
@@ -186,38 +217,63 @@ def main():
 
 
 
-
-                    
-
                 # infer breakpoint pos
                 left_pos = read.reference_start + 1 # 0-based leftmost coordinate
                 cigar_tuple = read.cigartuples
                 
-                ref_consume_len = 0
-                for i in cigar_tuple:
-                    if i[0] == 0 or i[0] == 2 or i[0] == 3 or i[0] == 7 or i[0] == 8:
-                        ref_consume_len += i[1]
+                if cigar_tuple[0][0] == 4:
+                    # 50S100M
+                    bnd_pos = left_pos
+                elif cigar_tuple[-1][0] == 4:
+                    # 100M50S
+                    # cal ref consume
+                    ref_consume_len = 0
+                    for i in cigar_tuple:
+                        if i[0] == 0 or i[0] == 2 or i[0] == 3 or i[0] == 7 or i[0] == 8:
+                            ref_consume_len += i[1]
 
-                bnd_pos = left_pos + ref_consume_len - 1 + 1
+                    bnd_pos = left_pos + ref_consume_len - 1 + 1
+                else:
+                    # skip
+                    continue
 
-                print(read.query_name,r1r2,read.reference_name,bnd_pos,cigarS,read_rev,sa_tag)
+                # 比较sv给出的断点位置与split read计算的断点位置
+                if abs(bnd_pos -hgene_bp) <= 10:
+                    # 断点位置符合
+                    bnd_pos_diff = 'PASS'
+                else:
+                    bnd_pos_diff = 'NOPASS'
+
+
+
+
+                if sr_mapq >= args.mapq:
+                    sr_mapq_check = 'PASS'
+                else:
+                    sr_mapq_check = 'NOPASS'
+
+                if bnd_pos_diff == 'PASS' and sr_mapq_check == 'PASS':
+                    final_check = 'PASS'
+                else:
+                    final_check = 'NOPAS'
+
+                # fusion gene | gene | gene_type | read name | READ1/2 | chr | sv_pos | split_read_pos | pos_check | SR MAPQ | mapQ check | final result (check pos and mapq)
+                ofFH.write('\t'.join([arr[1],hgene,'Hgene',read.query_name,r1r2,str(read.reference_name),str(hgene_bp),str(bnd_pos),bnd_pos_diff,str(read.mapping_quality),str(sr_mapq),sr_mapq_check,final_check])+'\n')
 
         
 
 
-
-
-
-
+        tgene_strand = gene_strand[tgene]
+        print("[INFO]: check Tgene %s (%s) info..." % (tgene,tgene_strand))
         
-        print("[INFO]: check Tgene %s info..." % (tgene))
         
         tgene_bp = fs_info[tgene]
         tgene_left = tgene_bp - 10
         tgene_right = tgene_bp + 10
+        #print(tgene,tgene_left,tgene_right)
+        partner_chr = fs_chr[hgene]
 
-
-        for read in samfile.fetch(arr[11],tgene_left,tgene_right):
+        for read in samfile.fetch(arr[18],tgene_left,tgene_right):
             if read.is_read1:
                 r1r2 = 'READ1'
             else:
@@ -245,10 +301,14 @@ def main():
                     continue
 
                 # skip mapQ < 10
-                mapQ = int(sa_tag.split(',')[4])
-                if mapQ < 10:
+                sr_mapq = int(sa_tag.split(',')[4])
+                if sr_mapq < 10:
                     continue
 
+                # check SA's chr
+                sa_chr = sa_tag.split(',')[0]
+                if sa_chr != partner_chr:
+                    continue
 
                 # check Tgene's sup read's ori
                 if hgene_strand == '+' and tgene_strand == '-':
@@ -340,19 +400,60 @@ def main():
                         else:
                             continue
 
-                
-
                 # infer breakpoint pos
                 left_pos = read.reference_start + 1 # 0-based leftmost coordinate
                 cigar_tuple = read.cigartuples
-                ref_consume_len = 0
-                for i in cigar_tuple:
-                    if i[0] == 0 or i[0] == 2 or i[0] == 3 or i[0] == 7 or i[0] == 8:
-                        ref_consume_len += i[1]
+                #print(cigar_tuple)
+                if cigar_tuple[0][0] == 4:
+                    # 50S100M
+                    bnd_pos = left_pos
+                elif cigar_tuple[-1][0] == 4:
+                    # 100M50S
+                    # cal ref consume
+                    ref_consume_len = 0
+                    '''
+                    M   BAM_CMATCH      0
+                    I   BAM_CINS        1
+                    D   BAM_CDEL        2
+                    N   BAM_CREF_SKIP   3
+                    S   BAM_CSOFT_CLIP  4
+                    H   BAM_CHARD_CLIP  5
+                    P   BAM_CPAD        6
+                    =   BAM_CEQUAL      7
+                    X   BAM_CDIFF       8
+                    B   BAM_CBACK       9
+                    '''
+                    for i in cigar_tuple:
+                        if i[0] == 0 or i[0] == 2 or i[0] == 3 or i[0] == 7 or i[0] == 8:
+                            ref_consume_len += i[1]
 
-                bnd_pos = left_pos + ref_consume_len - 1 + 1
-                print(read.query_name,r1r2,read.reference_name,bnd_pos,cigarS,read_rev,sa_tag)
+                    bnd_pos = left_pos + ref_consume_len - 1 + 1
+                else:
+                    # skip
+                    continue
 
+
+                # 比较sv给出的断点位置与split read计算的断点位置
+                if abs(bnd_pos -tgene_bp) <= 10:
+                    # 断点位置符合
+                    bnd_pos_diff = 'PASS'
+                else:
+                    bnd_pos_diff = 'NOPASS'
+
+
+
+                if sr_mapq >= args.mapq:
+                    sr_mapq_check = 'PASS'
+                else:
+                    sr_mapq_check = 'NOPASS'
+
+                if bnd_pos_diff == 'PASS' and sr_mapq_check == 'PASS':
+                    final_check = 'PASS'
+                else:
+                    final_check = 'NOPAS'
+
+                # fusion gene | gene | gene_type | read name | READ1/2 | chr | sv_pos | split_read_pos | pos_check | SR MAPQ | mapQ check | final result (check pos and mapq)
+                ofFH.write('\t'.join([arr[1],tgene,'Tgene',read.query_name,r1r2,str(read.reference_name),str(tgene_bp),str(bnd_pos),bnd_pos_diff,str(read.mapping_quality),str(sr_mapq),sr_mapq_check,final_check])+'\n')
 
 
 
